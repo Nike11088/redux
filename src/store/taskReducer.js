76f711519adfe62ed72526e289f5748e41b11743
Nikita Abakumov <nabakumov@mt.1tv.ru>
2022-06-30 12:30:22 +0300

    init

---
+++ src/store/taskReducer.js
@@ -0,0 +1,23 @@
+import { taskUpdated, taskRemoved } from './actionTypes'
+
+export function taskReducer(state = [], action) {
+  switch (action.type) {
+    case taskUpdated: {
+      const newArray = [...state]
+      const elementIndex = newArray.findIndex(
+        (el) => el.id === action.payload.id
+      )
+      newArray[elementIndex] = {
+        ...newArray[elementIndex],
+        ...action.payload,
+      }
+      return newArray
+    }
+    case taskRemoved: {
+      const newArray = [...state].filter((t) => t.id !== action.payload.id)
+      return newArray
+    }
+    default:
+      return state
+  }
+}
